558ab01ce6ebd35e9aca1f8fc9fa270334264f1e
theredsilva <ste.silva@outlook.it>
2024-11-25 12:33:25 +0100

    lol

--- app3.py
+++ app3.py
@@ -1,23 +1,37 @@
 import streamlit as st
 import pandas as pd
+import xlsxwriter
+import io
 
+def convert_to_excel(df):
+    output = io.BytesIO()
+    writer = pd.ExcelWriter(output, engine="xlsxwriter")
+    df.to_excel(writer, sheet_name="data")
+    # see: https://xlsxwriter.readthedocs.io/working_with_pandas.html
+    writer.close()
+    return output.getvalue()
 
 def main():
-    st.title("Caricamento File CSV/XLSX")
-    uploaded_file = st.file_uploader("Scegli un file CSV o XLSX", type=['csv', 'xlsx'])
-
+    st.title("Data Transformation")
+    uploaded_file = st.file_uploader("Choose a file",type={"xlsx"})
     if uploaded_file is not None:
-        # Verifica l'estensione del file
-        if uploaded_file.name.endswith('.csv'):
-            df = pd.read_csv(uploaded_file)
-            st.success("File CSV caricato con successo!")
-        elif uploaded_file.name.endswith('.xlsx'):
-            df = pd.read_excel(uploaded_file)
-            st.success("File XLSX caricato con successo!")
-        else:
-            st.error("Formato file non supportato!")
-
+        ###### transformation #####################################
+        df = pd.read_excel(uploaded_file)
         st.dataframe(df)
 
+
+        if st.button('Start Processing', help="Process Dataframe"):
+            st.header('Addes Column')
+            df['new_col'] = 1
+            st.dataframe(df)
+            st.balloons()
+            st.download_button(
+                                label="download as Excel-file",
+                                data=convert_to_excel(df),
+                                file_name="data.xlsx",
+                                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                key="excel_download",
+                                )
+                
 if __name__ == "__main__":
     main()
